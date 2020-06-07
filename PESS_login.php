0ebd6815d003eb4102168a4b3959a35ccaab6780
Minteck18 <66474922+Minteck18@users.noreply.github.com>
2020-06-07 16:04:49 +0800

    Add files via upload

--- PESS_login.php
+++ PESS_login.php
@@ -1,27 +1,54 @@
 <?php
-    function checkRequired($fieldName) {
-        $login_click = isset($_POST['login']);
-        if($login_click == true) {
-            $Username = $_POST['Username'];
-            if(empty($Username) == true) {
-                echo "<span style='color:red'>Required.</span>";
-            }
+if(isset($_SESSION) == false) {
+    session_start();
+}
+$has_Cookie_DisplayName = isset($_COOKIE["COOKIE_DISPLAYNAME"]);
+if($has_Cookie_DisplayName == true)
+{
+    $_SESSION['SESS_DISPLAYNAME'] = $_COOKIE['COOKIE_DISPLAYNAME'];
+}
+
+if(isset($_SESSION['SESS_DISPLAYNAME']) == true ) {
+    header('Location: logcall.php');
+}
+
+$isLoginButtonClicked = isset($_POST['login']);
+if($isLoginButtonClicked == true) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if($username == 'Michael' && $password=='Password')
+    {
+        $_SESSION['SESS_DISPLAYNAME'] = 'Michael';
+        
+        $rememberMeChecked = isset($_POST['RememberMe']);
+        if($rememberMeChecked == true) {
+            $expiryTime = time() + 60 * 60 * 24 * 30;
+            setcookie("COOKIE_DISPLAYNAME", "Michael" , $expiryTime);
         }
+        header('Location: logcall.php');
     }
-    $login_click = isset($_POST['submit']);
-    $allFieldsFilled = false;
-    if($login_click == true) {
-        if(empty($_POST['Username'] == false) &&
-           empty($_POST['Password'] == false)
-            ) {
-            $allFieldsFilled = true;
-        }
-        if($allFieldsFilled == true) {
-            echo 'Welcome ' ;
-            echo $_POST['Username'] . ', ';
-            echo 'Your password is ' . $_POST['Password'];
-        }
+    else {
+        echo "<span style='color:red'>Wrong Username / password </span>";
     }
+}
+$has_Cookie_DisplayName = isset($_COOKIE['COOKIE_DISPLAYNAME']);
+if($has_Cookie_DisplayName == true)
+{
+    $_cookie_DisplayName = $_COOKIE["COOKIE_DISPLAYNAME"];
+    echo "Welcome <strong>" . $_cookie_DisplayName . " ! </strong> [<a href='PESS_logout.php'>logout</a>]";
+}
+else {
+    if(isset($_SESSION) == false) {
+        session_start();
+    }
+    $has_Session_DisplayName = isset($_SESSION['SESS_DISPLAYNAME']);
+    if($has_Session_DisplayName == true)
+    {
+        $session_DisplayName = $_SESSION['SESS_DISPLAYNAME'];
+        echo "Welcome <strong>" . $session_DisplayName . " ! </strong> [<a href='PESS_logout.php'>logout</a>]";
+    }
+}
 ?>
 
 <!doctype html>
@@ -31,6 +58,15 @@
 <title>PESS_login</title>
  <link href="css/bootstrap-4.3.1.css" rel="stylesheet" type="text/css">
    <style type="text/css">
+   form {
+   color: #2F2FA2;
+   }
+   input {
+   color: #fc4445;
+   }
+   p {
+   color: #afd275;
+   }
     </style>
 </head>
 <body>
@@ -42,13 +78,23 @@
 		<p>Please enter your username and password
 		to login into the website. 
 		</p>
-<form action="logcall.php" method="post" id="pess_login">
-Username: <input name="Username" type="text" id="Username" required>
-<?php checkRequired('Username');?>
+<form action="logcall.php" method="post" name="PESS_login" onSubmit="return validateForm()">
+Username: <input name="Username" type="text" id="Username">
 Password: <input type="password" name="Password" id="Password" required></input>
-<?php checkRequired('Password');?>
 <input name="login" type="submit" id="login" value="login"></form>
 <input type="checkbox" name="RememberMe" id="RememberMe" value="No">Remember Me
 </div>
+<script type="text/javascript">
+function validateForm()
+{
+	var x=document.forms["PESS_login"]["Username"] .value;
+	if (x==null || x=="")
+  	{
+  		alert("User Name is required.");
+  		return false;
+  	}
+}
+</script>
 </body>
 </html>
+
