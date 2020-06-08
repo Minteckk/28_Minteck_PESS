b2bec82e377fe2974521acbaa1896d20e3adc70c
Minteck18 <66474922+Minteck18@users.noreply.github.com>
2020-06-08 15:29:43 +0800

    Add files via upload

--- PESS_login.php
+++ PESS_login.php
@@ -1,55 +1,28 @@
 <?php
 if(isset($_SESSION) == false) {
-    session_start();
-}
-$has_Cookie_DisplayName = isset($_COOKIE["COOKIE_DISPLAYNAME"]);
-if($has_Cookie_DisplayName == true)
-{
-    $_SESSION['SESS_DISPLAYNAME'] = $_COOKIE['COOKIE_DISPLAYNAME'];
-}
-
-if(isset($_SESSION['SESS_DISPLAYNAME']) == true ) {
-    header('Location: logcall.php');
-}
-
-$isLoginButtonClicked = isset($_POST['login']);
-if($isLoginButtonClicked == true) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+        session_start();
     
-    if($username == 'Michael' && $password=='Password')
-    {
-        $_SESSION['SESS_DISPLAYNAME'] = 'Michael';
-        
-        $rememberMeChecked = isset($_POST['RememberMe']);
-        if($rememberMeChecked == true) {
-            $expiryTime = time() + 60 * 60 * 24 * 30;
-            setcookie("COOKIE_DISPLAYNAME", "Michael" , $expiryTime);
+$isloginClicked = isset($_POST['login']);
+ if($isloginClicked == true) {
+ $Username = $_POST['Username'];
+ $Password = $_POST['Password'];
+    
+    if($Username == 'Username' && $Password=='Password')
+     {
+     $RememberMeChecked = isset($_POST['RememberMe']);
+     if($RememberMeChecked == true) {
+       $expiryTime = time() + 60 * 60 * 24 * 30;
+        setcookie("COOKIE_DISPLAYNAME", "Michael" , $expiryTime);
         }
         header('Location: logcall.php');
-    }
-    else {
-        echo "<span style='color:red'>Wrong Username / password </span>";
-    }
-}
-$has_Cookie_DisplayName = isset($_COOKIE['COOKIE_DISPLAYNAME']);
-if($has_Cookie_DisplayName == true)
-{
-    $_cookie_DisplayName = $_COOKIE["COOKIE_DISPLAYNAME"];
-    echo "Welcome <strong>" . $_cookie_DisplayName . " ! </strong> [<a href='PESS_logout.php'>logout</a>]";
-}
-else {
-    if(isset($_SESSION) == false) {
-        session_start();
-    }
-    $has_Session_DisplayName = isset($_SESSION['SESS_DISPLAYNAME']);
-    if($has_Session_DisplayName == true)
-    {
-        $session_DisplayName = $_SESSION['SESS_DISPLAYNAME'];
-        echo "Welcome <strong>" . $session_DisplayName . " ! </strong> [<a href='PESS_logout.php'>logout</a>]";
+        }
+   else {
+      echo "<span style='color:red'>Wrong Username / password </span>";
+        }
     }
 }
 ?>
+
 
 <!doctype html>
 <html>
@@ -81,7 +54,7 @@ else {
 <form action="logcall.php" method="post" name="PESS_login" onSubmit="return validateForm()">
 Username: <input name="Username" type="text" id="Username">
 Password: <input type="password" name="Password" id="Password" required></input>
-<input name="login" type="submit" id="login" value="login"></form>
+<input name="login" type="submit" id="login" value="Login"></form>
 <input type="checkbox" name="RememberMe" id="RememberMe" value="No">Remember Me
 </div>
 <script type="text/javascript">
